3397b4574082a0bf896387968a7f4726bbf11b92
Kobumanzi Trishia <kobu2rsha@gmail.com>
2026-07-16 12:30:33 +0300

    Enabled CORS in the backene

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,9 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  console.log('===== BACKEND STARTING - BUILD CHECK 001 =====');
+  console.log('BACKEND STARTING - BUILD CHECK 001');
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: (process.env.CORS_ORIGIN || '*').split(',') });
+  app.enableCors({
+
+    origin: [
+      'http://localhost:5173', 
+      'https://rukatrack-frontend.onrender.com' 
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, 
+  });
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT || 4000);
 }
